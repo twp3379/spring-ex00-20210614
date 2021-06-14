fd49fc67031d643f072b1e327b9fc01315957beb
twp3379 <wkakzks@naver.com>
2021-06-14 20:48:29 +0900

    first

---
+++ src/main/java/lombok/test/Desk.java
@@ -0,0 +1,16 @@
+package lombok.test;
+
+import lombok.EqualsAndHashCode;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+
+@ToString
+@EqualsAndHashCode
+@RequiredArgsConstructor
+public class Desk {
+	private final double price;
+	private final String model;
+	private String name;
+
+}
