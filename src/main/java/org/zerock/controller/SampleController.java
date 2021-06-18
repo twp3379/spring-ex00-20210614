252870fb99b9c9cecc9cbf54da7f8440c4492706
admin <admin@172.30.1.22>
2021-06-18 16:01:47 +0900

    2021.06.18

--- src/main/java/org/zerock/controller/SampleController.java
+++ src/main/java/org/zerock/controller/SampleController.java
@@ -1,18 +1,23 @@
 package org.zerock.controller;
 
-import java.util.Date;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 
 import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.zerock.domain.SampleDTO;
 import org.zerock.domain.TodoDTO;
 
@@ -92,6 +97,51 @@ public class SampleController {
 	public void initBinder(WebDataBinder binder) {
 		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 		binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, false));
+	}
+	
+	// 144쪽 
+	
+	@GetMapping("/ex05")
+	public void ex05() {
+		log.info("/ex05.............");
+	}
+	
+	// 146쪽 
+	
+	@GetMapping("/ex06")
+	public @ResponseBody SampleDTO ex06() {
+		log.info("/ex06............");
+		
+		SampleDTO dto = new SampleDTO();
+		dto.setAge(10);
+		dto.setName("홍길동");
+		
+	    return dto;
+	}
+	
+	// 148쪽 
+	
+	@GetMapping("/ex07")
+	public ResponseEntity<String> ex07(){
+		
+		// {"name": "홍길동"}
+	   String msg = "{\"name\": \"홍길동\"}";
+	   
+	   HttpHeaders header = new HttpHeaders();
+	   header.add("Content-Type", "application/json;charset=UTF-8");
+	   
+	   return new ResponseEntity<String>(msg, header, HttpStatus.OK);
+	}
+	
+	// 140쪽 
+	@GetMapping("/ex04")
+	public String ex04(SampleDTO dto,@ModelAttribute("page") int page) {
+		
+		log.info("dto: " + dto);
+		log.info("page: " + page);
+		
+		return "/sample/ex04";
+		
 	}
 	
 }
