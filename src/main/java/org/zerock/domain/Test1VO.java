397ffd40615fec26e260062072a1a89cc6185baf
admin <admin@172.30.1.22>
2021-06-21 16:46:29 +0900

    프레임워크 데이터베이스 연

--- src/main/java/org/zerock/domain/Test1VO.java
+++ src/main/java/org/zerock/domain/Test1VO.java
@@ -3,10 +3,10 @@ package org.zerock.domain;
 import lombok.Data;
 
 @Data
-public class SampleDTO {
-	
+public class Test1VO {
 	private long id;
 	private String name;
 	private int age;
-
+	
+	
 }
---
+++ src/main/java/org/zerock/mapper/BoardMapper.java
@@ -0,0 +1,37 @@
+package org.zerock.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Select;
+import org.zerock.domain.BoardVO;
+
+public interface BoardMapper {
+	
+//	@Select("SELECT * FROM tbl_board")
+	public List<BoardVO> getList();
+	
+	
+	// INSERT INTO tbl_board (title, content, writer)
+	// VALUES (#{title}, #{content}, #{Writer})
+    public int insert(BoardVO board);
+    /*
+      String sql = "INSERT INTO tbl_board (title, content, writer) VALUES (?, ?, ?)";
+      ...
+      pstmt.setString(1, board.getTitle());
+      pstmt.setString(2, board.getContent();
+      pstmt.setString(3, board.getWriter();
+      
+       pstmt.executeUpdate();
+      
+     
+     */
+    
+    public int insertSelectKey(BoardVO board);
+    
+    public BoardVO read(long bno);
+    
+    public int delete(long bno);
+    
+    public int update(BoardVO board);
+   
+}
