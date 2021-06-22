76efbda53083fc2fb01a59a50255f2b91c455fe3
twp3379 <wkakzks@naver.com>
2021-06-22 16:46:55 +0900

    2021

---
+++ src/main/java/org/zerock/service/BoardServiceImpl.java
@@ -0,0 +1,48 @@
+package org.zerock.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.zerock.domain.BoardVO;
+import org.zerock.mapper.BoardMapper;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Service
+public class BoardServiceImpl implements BoardService{
+	
+	private BoardMapper mapper;
+	
+//	@Autowired
+//	public BoardServiceImpl(BoardMapper mapper) {
+//		this.mapper = mapper;
+//	}
+
+	@Override
+	public void register(BoardVO board) {
+		mapper.insertSelectKey(board);
+	}
+
+	@Override
+	public BoardVO get(Long bno) {
+		return mapper.read(bno);
+	}
+
+	@Override
+	public boolean modify(BoardVO board) {
+		return mapper.update(board) == 1;
+	}
+
+	@Override
+	public boolean remove(Long bno) {
+		return mapper.delete(bno) == 1;
+	}
+
+	@Override
+	public List<BoardVO> getList() {
+		return mapper.getList();
+	}
+	
+
+}
